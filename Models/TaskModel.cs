a53b07b68206ef45b777a38424dc020727e7a7c4
JarIsCoding <derpyderp1098@gmail.com>
2024-04-29 11:54:44 -0700

    DTOs and merge conflict fixes

--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -7,6 +7,26 @@ namespace tasksprintbe.Models
 {
     public class TaskModel
     {
-        
+        public int ID { get; set; }
+
+        public int UserId { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? DateCreated { get; set; }
+
+        public string? AssignedTo { get; set; }
+
+        public string? DateUpdated { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        TaskModel()
+        {
+
+        }
+
     }
 }
